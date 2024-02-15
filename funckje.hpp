6453982536af6e1664874122ee142e9aac50a82a
MMiochi <155749660+MMiochi@users.noreply.github.com>
2024-02-16 00:51:56 +0100

    Create funckje.hpp

---
+++ funckje.hpp
@@ -0,0 +1,19 @@
+int * tabela(int table_size);
+
+int * sortowanie_babelkowe(int table_size, int * table);
+
+int * sortowanie_wstawianie(int table_size, int * table);
+
+void szukanie_liniowe(int table_size, int * table, int searched);
+
+void szukanie_liniowe_z_wartownikiem(int table_size, int * table, int searched);
+
+int * sortowanie_koktajlowe(int table_size, int * table);
+
+void szukanie_interpolacyjne(int table_size, int* table, int searched);
+
+int szukanie_binarne(int table_size, int * table, int searched);
+
+void sortowanie_szybkie(int* table, int leftIndex, int rightIndex);
+
+void szukanie_skokowe(int table_size, int * table, int searched);
